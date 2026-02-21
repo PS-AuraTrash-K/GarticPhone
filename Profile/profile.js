83f2e2794b1e4b16f6039d021a5246dc0dabd39f
SashaOzga <sozgaozga1743@gmail.com>
2026-02-21 19:32:48 +0200

    profile

---
+++ Profile/profile.js
@@ -0,0 +1,7 @@
+let start = document.querySelector(".start-btn")
+
+
+start.addEventListener("click", function() {
+    let inpit = document.querySelector(".shoot")
+    console.log(inpit.value)
+})
