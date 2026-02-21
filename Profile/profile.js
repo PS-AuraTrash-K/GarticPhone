dbd333bef86ff3cae9966d693f29fc8940e4fedc
SashaOzga <sozgaozga1743@gmail.com>
2026-02-21 19:51:29 +0200

    ok

--- Profile/profile.js
+++ Profile/profile.js
@@ -4,4 +4,5 @@ let start = document.querySelector(".start-btn")
 start.addEventListener("click", function() {
     let inpit = document.querySelector(".shoot")
     console.log(inpit.value)
+    window.location.href = "../Loby/loby.html?name="+inpit.value
 })
